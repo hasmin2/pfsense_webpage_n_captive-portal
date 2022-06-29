68c0601ad0e4c5e6ba59988aa566f805971fa580
hasmin <hasmin2@gamil.com>
2022-06-29 14:29:13 +0900

    Add Default gateway set API for proper use.

---
+++ usr/local/www/custom_manual_timeperiod_check.php
@@ -0,0 +1,25 @@
+<?
+require_once("api/framework/APIModel.inc");
+require_once("api/framework/APIResponse.inc");
+if(isset($config['gateways']['manualroutetimestamp']) && isset($config['gateways']['manualrouteduration'])){
+	$date = new DateTime();
+	if((round($date->getTimestamp()/60,0) - $config['gateways']['manualroutetimestamp']) > $config['gateways']['manualrouteduration']){
+		unset($config['gateways']['manualroutetimestamp']);
+		unset($config['gateways']['manualrouteduration']);
+		write_config();
+		echo('back to auto routing due to duration is expire');
+	}
+	else {echo('still manual routing activated');}
+}
+else if (!isset($config['gateways']['manualroutetimestamp']) && !isset($config['gateways']['manualrouteduration'])){
+	echo('auto routing enabled, no action performed.');
+}
+else {
+	unset($config['gateways']['manualroutetimestamp']);
+	unset($config['gateways']['manualrouteduration']);
+	write_config();
+	echo('uncecessary setting for time duration, recovering back to auto-routing');
+}
+
+
+?>
