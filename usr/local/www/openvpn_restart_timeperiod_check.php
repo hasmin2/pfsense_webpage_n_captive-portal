4083c895713107d4911eba2315195e1659022ec4
hasmin <hasmin2@gamil.com>
2023-05-23 10:36:49 +0900

    Modified OpenVPN to have complete re-start if the server "vpn-server.synersat.noc" is not able to ping every minute.
    Added more servers to nc/ping.

--- usr/local/www/openvpn_restart_timeperiod_check.php
+++ usr/local/www/openvpn_restart_timeperiod_check.php
@@ -3,6 +3,16 @@
 //require_once("api/framework/APIResponse.inc");
 require_once("openvpn.inc");
 global $config;
+
+$vpnclients = openvpn_get_active_clients();
+foreach($vpnclients as $vpnclient){
+    $pingresult = mwexec('ping -c1 -t5 -S '.$vpnclient['virtual_addr'].' vpn-server.synersat.noc > /dev/null')==0 ? "online": "offline";
+}
+
+if ($pingresult == "offline"){
+	$config['openvpn']['openvpnrestart']="";
+}
+
 if(isset($config['openvpn']['openvpnrestart'])){
 	$clients = openvpn_get_active_clients();
 
