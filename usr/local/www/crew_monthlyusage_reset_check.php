e1768cc58f04c9f0963607504c57c879f77ff077
hasmin <hasmin2@gamil.com>
2023-06-01 17:02:54 +0900

    Fixed problem not apply crew usage reset logic.

---
+++ usr/local/www/crew_monthlyusage_reset_check.php
@@ -0,0 +1,18 @@
+<?
+require_once("captiveportal.inc");
+init_config_arr(array('captiveportal'));
+$cpzone="crew";
+global $config;
+
+
+$cpdb = captiveportal_read_db();
+$resetdata = array( "varusersresetquota" => "true");
+$updateflag = array( "varusersmodified" => "update");
+$usercount = count ($config["installedpackages"]["freeradius"]["config"]);
+echo $updateflag['varusersmodified'];
+for ($i=0; $i < $usercount; $i++){
+	$config["installedpackages"]["freeradius"]["config"][$i]["varusersresetquota"]="true";
+	$config["installedpackages"]["freeradius"]["config"][$i]["varusersmodified"]="update";
+	write_config("Reset Crew wifi usage");
+}
+?>
