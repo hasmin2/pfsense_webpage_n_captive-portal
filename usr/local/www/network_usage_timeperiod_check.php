af771fa99134aea03c806a39472f339707ae1cf3
hasmin <hasmin2@gamil.com>
2023-11-09 12:33:45 +0900

    Added Network State widget and its function
    Modified to have GW "" on Routing is "Automatic" to avoid omitting to operators.

--- usr/local/www/network_usage_timeperiod_check.php
+++ usr/local/www/network_usage_timeperiod_check.php
@@ -61,12 +61,13 @@ foreach (json_decode($json_string, true)["interfaces"] as $value) {
             $config['gateways']['gateway_item'][$key]['speedrx']=round ($value['traffic']['fiveminute'][0]['rx']/38400,0);
 		    $currentusagegb = floatval($config['gateways']['gateway_item'][$key]['currentusage']);
             $currentusagegb += floatval(round(($value['traffic']['fiveminute'][0]['rx'] + $value['traffic']['fiveminute'][0]['tx'])/1000000000, 6));
+            sleep(1);
 	        echo ("time:".time()."  CurrentUsage : ".$currentusagegb."  Usage: ".round(($value['traffic']['fiveminute'][0]['rx'] + $value['traffic']['fiveminute'][0]['tx'])/1000000000, 6)." LastUsage:".$config['gateways']['gateway_item'][$key]['currentusage']."\n");
 	        $config['gateways']['gateway_item'][$key]['currentusage'] = $currentusagegb;
         }
 	}
 }
-sleep (3);
+sleep (1);
 write_config("networkusage update");
 
 $datastring = rtrim($datastring, ',');
