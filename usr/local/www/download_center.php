690dbd08100e3a11f03a4c58d26252edb98fe930
Roudy.Ha <hasmin2@gmail.com>
2025-04-16 18:21:04 +0900

    -- Update 1.0.70
      --Fixed
        Revive login id/password as customer/segans
    
      --Fixed
        Main_Dashboard shows intermediate "internal error" is now being ignored (Not seen any more)
    
      --Fixed
        Main Dashboard (Old main screen) is now being able to edit widgets.
        CREW WIFI checkbox was turn to uncheck every 10 secs.
    
      --Fixed
        CREW Voucher is now being deleted correctly begin of the month once after it was used up.
    
      -- New Feature
      New button available on FLEET DASHBOARD to reset all VSAT IDs and even don't need to wait firewall is online. just click.

--- usr/local/www/download_center.php
+++ usr/local/www/download_center.php
@@ -20,6 +20,8 @@ function print_download_link($title, $image, $filename){
 
 	$help_menu[] = array(gettext("TeamViewer Quick Support"), "TMV.DE.png", "TeamViewerQS.exe");
     array_push($help_menu, array(gettext("AnyDesk Remote Support"), "anydesk.png", "AnyDesk.exe"));
+    array_push($help_menu, array(gettext("WaveSync Guide - English"), "manual.png", "SmartBox User Guide-Kor.pdf"));
+    array_push($help_menu, array(gettext("WaveSync Guide - Korean"), "manual.png","SmartBox User Guide-Eng.pdf"));
 	array_push($help_menu, array(gettext("FX Guide Book - English"), "manual.png", "Fleet Xpress User guide latest-Eng.pdf"));
 	array_push($help_menu, array(gettext("FX Guide Book - Korean"), "manual.png","Fleet Xpress User guide latest-Kor.pdf"));
     array_push($help_menu, array(gettext("Bluewave Mail Guide Book - English"), "manual.png","Bluewave Mail User guide latest-Eng.pdf"));
