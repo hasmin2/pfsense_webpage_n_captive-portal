38cfea7dd9e7270cf0709860964dfd6b32363323
Roudy.Ha <hasmin2@gmail.com>
2026-01-03 23:17:46 +0900

    Bluewave-Link Changelog ============================================
    Update 1.0.90
     -- NEW FEATURE
        Schedule can be disabled from UI
    
        WIFI user modification is now available.
    
     -- FIX
        Schedule suspension is now working as desired.

--- usr/local/www/crew_usage_timeperiod_check.php
+++ usr/local/www/crew_usage_timeperiod_check.php
@@ -1,9 +1,13 @@
-<?
-require_once("captiveportal.inc");
-init_config_arr(array('captiveportal'));
-$cpzone="crew";
-$cpdb = captiveportal_read_db();
-foreach ($cpdb as $eachuser){
-	portal_reply_page("/","connected","Refreshing",$eachuser[3],$eachuser[2],$eachuser[4]);
-}
+<?php
+    require_once("captiveportal.inc");
+    init_config_arr(array('captiveportal'));
+
+    $cpzone = "crew";
+    $cpdb = captiveportal_read_db();
+
+    foreach ($cpdb as $eachuser) {
+        if(get_suspend_timeschedule($eachuser[4])){
+            captiveportal_disconnect_client($eachuser[5]);
+        }
+    }
 ?>
